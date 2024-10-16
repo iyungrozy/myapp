de48e0f71b4240540fbfd95abfee33577fbcdf1c
fpf.iyung <fpf.iyung@gmail.com>
2024-10-16 14:53:56 +0000

    Lets Begin Android Journey , menyala abangkuh 🗿🗿🗿🗿🗿🗿🗿

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:myapp/config/config.dart';
+import 'package:myapp/presentation/screens/main_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:motion/motion.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Motion.instance.initialize();
+  Motion.instance.setUpdateInterval(45.fps);
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Banking App',
+      theme: themeData(context),
+      home: const MainScreen(),
+    );
+  }
+}
